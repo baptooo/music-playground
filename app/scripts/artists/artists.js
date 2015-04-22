8d356a0b08134fcb934489cb5193e0b905055b3a
Baptiste Rios-Campo <baptiste.rios-campo@fullsix.com>
2015-04-22 21:20:00 +0200

    refactor: currentArtist
    was totaly stupid...

--- app/scripts/artists/artists.js
+++ app/scripts/artists/artists.js
@@ -2,19 +2,24 @@ function ArtistsCtrl(artistPromise, $rootScope) {
   var artists = this;
 
   artists.items = artistPromise;
+  artists.updateCurrentArtist = _updateCurrentArtist;
 
-  $rootScope.$watch('currentArtistName', function (value) {
-    var dataLen = artists.items.length;
-    if (!value || !dataLen) {
-      return true;
-    }
-    for (var i = 0; i < dataLen; i++) {
-      if (artists.items[i].label == value) {
-        $rootScope.currentArtist = artists.items[i];
-        break;
-      }
-    }
-  });
+  function _updateCurrentArtist(artist) {
+    $rootScope.currentArtist = artist;
+  }
+
+  //$rootScope.$watch('currentArtistName', function (value) {
+  //  var dataLen = artists.items.length;
+  //  if (!value || !dataLen) {
+  //    return true;
+  //  }
+  //  for (var i = 0; i < dataLen; i++) {
+  //    if (artists.items[i].label == value) {
+  //      $rootScope.currentArtist = artists.items[i];
+  //      break;
+  //    }
+  //  }
+  //});
 }
 
 module.exports = ArtistsCtrl;
